18567fcaaf790deeb0fa07baa2e6eed498835c2a
Aurelio Barrios <aubarrio@ucsd.edu>
2020-11-15 14:50:59 -0800

    Checkpoint

---
+++ run.py
@@ -0,0 +1,23 @@
+from src.etl import complete
+import json
+import sys
+
+def main():
+    if targets:
+        if targets[0] == "cora":
+            with open('config/cora_params.json') as fh:
+                data_cfg = json.load(fh)
+        elif targets[0] == "twitch":
+            with open('config/twitch_params.json') as fh:
+                data_cfg = json.load(fh)
+        else:
+            with open('config/cora_params.json') as fh:
+                data_cfg = json.load(fh)
+    else:
+        with open('config/cora_params.json') as fh:
+            data_cfg = json.load(fh)
+    complete(**data_cfg)
+
+if __name__ == '__main__':
+    targets = sys.argv[1:]
+    main()
